fe375fcd0bb59b7fae25773b54846ad0840c8550
Pranshu Chaudhary <wittywit@users.noreply.github.com>
2018-01-15 01:00:17 +0530

    Add files via upload

--- demo/main.js
+++ demo/main.js
@@ -32,3 +32,42 @@ $( function() {
 $( function() {
     $( "#player11" ).draggable();
   } );
+
+
+
+//================= Player 2 ======================
+
+$( function() {
+    $( "#player01" ).draggable();
+  } );
+
+$( function() {
+    $( "#player02" ).draggable();
+  } );
+$( function() {
+    $( "#player03" ).draggable();
+  } );
+$( function() {
+    $( "#player04" ).draggable();
+  } );
+$( function() {
+    $( "#player05" ).draggable();
+  } );
+$( function() {
+    $( "#player06" ).draggable();
+  } );
+$( function() {
+    $( "#player07" ).draggable();
+  } );
+$( function() {
+    $( "#player08" ).draggable();
+  } );
+$( function() {
+    $( "#player09" ).draggable();
+  } );
+$( function() {
+    $( "#player010" ).draggable();
+  } );
+$( function() {
+    $( "#player011" ).draggable();
+  } );
